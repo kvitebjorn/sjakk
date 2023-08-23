de8af3a889355089ce68684ca859be5f124d2981
kvitebjorn <harintonkairu@gmail.com>
2023-08-22 19:22:04 -0600

    add get bit tests

--- sjakk-game/board-tests/bitboard/BitBoard.fs
+++ sjakk-game/board-tests/bitboard/BitBoard.fs
@@ -13,6 +13,43 @@ let CreateEmptyBitBoard () =
     Assert.NotNull(emptyBitBoard)
 
 [<Test>]
+let GetBit () =
+    // 0000
+    let zero : System.UInt64 = 0UL
+    Assert.IsFalse(zero.get(0))
+    Assert.IsFalse(zero.get(1))
+    Assert.IsFalse(zero.get(2))
+    Assert.IsFalse(zero.get(3))
+
+    // 0001
+    let one : System.UInt64 = 1UL
+    Assert.IsTrue(one.get(0))
+    Assert.IsFalse(one.get(1))
+    Assert.IsFalse(one.get(2))
+    Assert.IsFalse(one.get(3))
+
+    // 0010
+    let two : System.UInt64 = 2UL
+    Assert.IsFalse(two.get(0))
+    Assert.IsTrue(two.get(1))
+    Assert.IsFalse(two.get(2))
+    Assert.IsFalse(two.get(3))
+
+    // 0100
+    let four : System.UInt64 = 4UL
+    Assert.IsFalse(four.get(0))
+    Assert.IsFalse(four.get(1))
+    Assert.IsTrue(four.get(2))
+    Assert.IsFalse(four.get(3))
+
+    // 1000
+    let eight : System.UInt64 = 8UL
+    Assert.IsFalse(eight.get(0))
+    Assert.IsFalse(eight.get(1))
+    Assert.IsFalse(eight.get(2))
+    Assert.IsTrue(eight.get(3))
+
+[<Test>]
 let SetPawnOnE2 () =
     let bitBoard = new BitBoard()
 
